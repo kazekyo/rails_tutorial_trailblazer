34c8d1bb127f4ee798309d09f82782756e11e431
Kyohei Nakamori <k.hukusui@gmail.com>
2017-07-28 14:59:01 +0900

    updateで使っているので消しちゃダメだった

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -61,6 +61,12 @@ class UsersController < ApplicationController
   end
   
   private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
+
     # Before filters
 
     # Confirms the correct user.
